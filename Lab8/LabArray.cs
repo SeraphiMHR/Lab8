5b1fdaa0965480a461f8ef936c1faea4f21497e4
Akimov A <akimov1dev@gmail.com>
2020-01-08 16:51:23 +0300

    Добавьте файлы проекта.

---
+++ Lab8/LabArray.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    class LabArray<T> : System.Collections.Generic.IList<T>
+    {
+        private T[] __arr;
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= Count || index < 0)
+                    throw new ArgumentOutOfRangeException();
+                return __arr[index];
+            }
+            set
+            {
+                if (index >= Count || index < 0)
+                    throw new ArgumentOutOfRangeException();
+                __arr[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get => __arr.Length;
+        }
+
+        public bool IsReadOnly
+        {
+            get => __arr.IsReadOnly;
+        }
+
+        public void Add(T item)
+        {
+            if(!IsReadOnly)
+            {
+                T[] temp = new T[__arr.Length];
+                temp = __arr;
+
+                __arr = new T[temp.Length + 1];
+
+                for (int i = 0; i < temp.Length; i++)
+                    __arr[i] = temp[i];
+
+                __arr[__arr.Length - 1] = item;
+            }
+
+            throw new NotSupportedException();
+
+        }
+
+        public void Clear()
+        {
+            if (!IsReadOnly)
+                __arr = new T[0];
+            throw new NotSupportedException();
+                
+        }
+
+        public bool Contains(T item)
+        {
+            return __arr.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (arrayIndex < 0)
+                throw new ArgumentOutOfRangeException();
+            if (array.Length - arrayIndex - 1 < __arr.Length)
+                throw new ArgumentException();
+
+            for (int i = arrayIndex; i < array.Length; i++)
+                array[i] = __arr[i];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (IEnumerator<T>)__arr.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            for(int i = 0; i < __arr.Length; i++)
+            {
+                if (__arr[i].Equals(item))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index >= __arr.Length)
+                throw new ArgumentOutOfRangeException();
+
+            if (IsReadOnly)
+                throw new NotSupportedException();
+
+            __arr[index] = item;
+        }
+
+        public bool Remove(T item)
+        {
+            for(int i = 0; i < __arr.Length; i++)
+            {
+                if(__arr[i].Equals(item))
+                {
+                    for(int j = i; j < __arr.Length - 1; j++)
+                    {
+                        __arr[j] = __arr[j + 1];
+                    }
+
+                    T[] temp = new T[__arr.Length];
+                    temp = __arr;
+                    __arr = new T[__arr.Length - 1];
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= __arr.Length)
+                throw new ArgumentOutOfRangeException();
+            if (IsReadOnly)
+                throw new NotSupportedException();
+
+            for (int i = index; i < __arr.Length - 1; i++)
+                __arr[i] = __arr[i + 1];
+
+            T[] temp = new T[__arr.Length];
+            temp = __arr;
+
+            __arr = new T[__arr.Length - 1];
+            __arr = temp;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+           return  __arr.GetEnumerator();
+        }
+    }
+}
---
+++ Lab8/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace Lab8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isNumber = true;
+        private ArrayList numbers = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttonStartWordPad.Click += buttonStartWordPad_Click;
+            TextBoxForNumbers.KeyDown += TextBoxForNumbers_KeyDown;
+            TextBoxForNumbers.PreviewKeyDown += TextBoxForNumbers_PreviewKeyDown;
+            buttonConvert.Click += buttonConvert_Click;
+        }
+
+        private void buttonConvert_Click(object sender, RoutedEventArgs e)
+        {
+            if (numbers != null)
+            {
+                double element;
+
+                if(double.TryParse(TextBoxForNumbers.Text, out element))
+                    numbers.Add(element);
+                
+                using(StreamWriter streamWriter = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "result.txt", false, Encoding.UTF8))
+                {
+                    foreach (var val in numbers)
+                        streamWriter.WriteLine(val.ToString());
+                }
+                return;
+            }
+
+            MessageBox.Show("You didn't start wordpad or save text");
+
+        }
+
+        private void TextBoxForNumbers_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key < Key.D0 || e.Key > Key.D9)
+            {
+                if(e.Key != Key.Back && e.Key != Key.OemMinus && e.Key != Key.OemPeriod)
+                {
+                    isNumber = false;
+                    return;
+                }
+            }
+
+            isNumber = true;
+        }
+
+        private void TextBoxForNumbers_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!isNumber)
+                e.Handled = true;
+        }
+
+        private void buttonStartWordPad_Click(object sender, RoutedEventArgs e)
+        {
+            Process wordpadProcess = new Process();
+            wordpadProcess.StartInfo.FileName = "wordpad.exe";
+            wordpadProcess.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+
+            wordpadProcess.Start();
+
+            wordpadProcess.WaitForExit();
+
+            var arr = new List<string>();
+            string line;
+
+            using (StreamReader stream = File.OpenText(AppDomain.CurrentDomain.BaseDirectory + "ex.txt"))
+            {
+                while ((line = stream.ReadLine()) != null)
+                    arr.Add(line);
+            }
+
+            string[] words = strRemLet(arr).Split(' ');
+            numbers = new ArrayList();
+
+            foreach(var val in words)
+            {
+                try
+                {
+                    numbers.Add(Convert.ToDouble(val));
+                }
+                catch(Exception E)
+                {
+                    //MessageBox.Show(E.Message);
+                    continue;
+                }
+            }
+
+
+
+        }
+
+        private string strRemLet(List<string> lines)
+        {
+            StringBuilder sym = new StringBuilder();
+            StringBuilder result = new StringBuilder();
+            Regex regexAlphabet = new Regex("[a-zа-я]+");
+            Regex regexSeparator = new Regex("[,]+");
+            Regex regexMinus = new Regex("[-]+");
+            MatchCollection matchCollection;
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                matchCollection = regexAlphabet.Matches(lines[i]);
+
+                foreach(var val in matchCollection)
+                {
+                    sym.Append(val.ToString());
+                }
+
+                lines[i] = regexSeparator.Replace(lines[i], ",");
+                lines[i] = regexAlphabet.Replace(lines[i], " ");
+                lines[i] = regexMinus.Replace(lines[i], "-");
+            }
+
+            using (StreamWriter streamWriter = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "strings.txt", false, Encoding.UTF8/*Encoding.GetEncoding("windows-1251")*/))
+            {
+                streamWriter.Write(sym.ToString());
+            }
+
+            foreach (var val in lines)
+                result.Append(val + " ");
+
+            return result.ToString();
+
+        }
+    }
+}
